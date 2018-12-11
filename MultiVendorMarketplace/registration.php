c84c43950a05094cb7000d37b2a230c58c7d1ffd
VivekJadaun <vivekjadaun08@gmail.com>
2018-12-11 14:05:34 +0530

    resolve code climate issues with registration.php

--- MultiVendorMarketplace/registration.php
+++ MultiVendorMarketplace/registration.php
@@ -1,7 +1,7 @@
 <?php
-  \Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Vinsol_MultiVendorMarketplace',
-    __DIR__
-  );
-?>
+    \Magento\Framework\Component\ComponentRegistrar::register(
+      \Magento\Framework\Component\ComponentRegistrar::MODULE,
+      'Vinsol_MultiVendorMarketplace',
+      __DIR__
+    );
+
