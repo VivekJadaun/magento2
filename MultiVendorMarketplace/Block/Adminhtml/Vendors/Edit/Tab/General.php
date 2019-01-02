96302353d25f39b16a6521db4de10ae66e3852bd
VivekJadaun <vivekjadaun08@gmail.com>
2019-01-02 14:05:11 +0530

    pre merge commit

---
+++ MultiVendorMarketplace/Block/Adminhtml/Vendors/Edit/Tab/General.php
@@ -0,0 +1,199 @@
+<?php
+  namespace Vinsol\MultiVendorMarketplace\Block\Adminhtml\Vendors\Edit\Tab;
+
+  /**
+   * 
+   */
+  class General extends \Magento\Backend\Block\Widget\Form\Generic implements \Magento\Backend\Block\Widget\Tab\TabInterface 
+	{
+		protected $wysiwygConfig;
+		protected $vendor;
+		protected $roles;
+		public function __construct(
+			\Magento\Backend\Block\Template\Context $context,
+			\Magento\Framework\Registry $registry,
+			\Magento\Framework\Data\FormFactory $formFactory,
+			\Magento\Cms\Model\Wysiwyg\Config $wysiwygConfig,
+			\Vinsol\MultiVendorMarketplace\Model\VendorFactory $vendorFactory,
+			\Vinsol\MultiVendorMarketplace\Model\Config\Source\Roles $roles,
+			array $data = []
+		) { 
+			$this->wysiwygConfig = $wysiwygConfig;
+			$this->vendor = $vendorFactory->create();
+			$this->roles = $roles->toOptionArray();
+			parent::__construct($context, $registry, $formFactory, $data);
+		}
+
+		protected function _prepareForm()
+		{
+			$model = $this->_coreRegistry->registry('vendor');
+			$form = $this->_formFactory->create();
+
+			$fieldset = $form->addFieldset(
+				'base_fieldset',
+				['legend' => __('General')]
+			);
+
+			if ($model->getId()) {
+				$fieldset->addField('id', 'hidden', ['name' => 'id']);
+			}
+
+			$fieldset->addField(
+				'username',
+				'text',
+				[
+					'name' => 'username',
+					'label' => __('Username'),
+					'required' => true
+				]
+			);
+
+			$fieldset->addField(
+				'password',
+				'password',
+				[
+					'name' => 'password',
+					'label' => __('Password'),
+					'required' => true
+				]
+			); 
+
+			$fieldset->addField(
+				'first_name',
+				'text',
+				[
+					'name' => 'first_name',
+					'label' => __('First Name'),
+					'required' => true
+				]
+			);
+
+			$fieldset->addField(
+				'last_name',
+				'text',
+				[
+					'name' => 'last_name',
+					'label' => __('Last Name'),
+					'required' => false
+				]
+			);
+
+			$fieldset->addField(
+				'role',
+				'select',
+				[
+					'name' => 'role',
+					'label' => __('Role'),
+					'required' => true,
+					'values' => $this->roles
+				]
+			); 
+
+			$fieldset->addField(
+				'active',
+				'select',
+				[
+					'name' => 'active',
+					'label' => __('Active'),
+					'required' => true,
+					'values' => [
+						['value'=>"1",'label'=>__('Yes')],
+						['value'=>"0",'label'=>__('No')]
+					]
+				]
+			);
+
+			$fieldset->addField(
+				'commission',
+				'text',
+				[
+					'name' => 'commission',
+					'label' => __('Commission (in %)'),
+					'required' => true
+				]
+			);
+
+			$fieldset->addField(
+				'contact_no',
+				'text',
+				[
+					'name' => 'contact_no',
+					'label' => __('Contact number'),
+					'required' => true
+				]
+			);
+
+			$fieldset->addField(
+				'email',
+				'text',
+				[
+					'name' => 'email',
+					'label' => __('Email'),
+					'required' => true
+				]
+			);      
+
+			$fieldset->addField(
+				'address',
+				'textarea',
+				[
+					'name' => 'address',
+					'label' => __('Address'),
+					'required' => false,
+					'style' => 'height: 10em; width: 100%;',
+					'config' => $this->wysiwygConfig->getConfig()
+				]
+			);     
+
+			$fieldset->addField(
+				'sort_order',
+				'text',
+				[
+					'name' => 'sort_order',
+					'label' => __('Sort Order'),
+					'required' => false,
+					'value' => '0'
+				]
+			);
+
+			$fieldset->addField(
+				'logo',
+				'file',
+				[
+					'name' => 'logo',
+					'label' => __('Logo'),
+					'required' => false
+				]
+			); 
+
+			$fieldset->addField(
+				'banner',
+				'file',
+				[
+					'name' => 'banner',
+					'label' => __('Banner'),
+					'required' => false
+				]
+			); 
+
+			$data = $model->getData();
+			$form->setValues($data);
+			$this->setForm($form);
+
+			return parent::_prepareForm();
+
+		}
+
+		public function getTabLabel(){
+			return __('Review');
+		}
+		public function getTabTitle(){
+			return __('Review');
+		}
+		public function canShowTab(){
+			return true;
+		}
+		public function isHidden(){
+			return false;
+		}
+	}
