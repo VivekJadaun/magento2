0a54d197e87c37e3d990bafec40017f743f517bb
VivekJadaun <vivekjadaun08@gmail.com>
2019-01-03 17:11:40 +0530

    schema modified

--- MultiVendorMarketplace/Setup/InstallSchema.php
+++ MultiVendorMarketplace/Setup/InstallSchema.php
@@ -31,162 +31,29 @@
 					],
 					'Entity ID'
 				)
-	    //   ->addColumn(
-					// 'first_name',
-					// \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-					// 255,
-					// [
-					// 'nullable' => false
-					// ],
-					// 'First Name'
-	    //   )
-	    //   ->addColumn(
-					// 'last_name',
-					// \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-					// 255,
-					// [
-					// 'nullable' => true
-					// ],
-					// 'Last Name'
-	    //   )
-				// ->addColumn(
-				// 	'display_name',
-				// 	\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-				// 	255,
-				// 	[
-				// 	'nullable' => false
-				// 	],
-				// 	'Display Name'
-				// )
-				// ->addColumn(
-				// 	'username',
-				// 	\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-				// 	255,
-				// 	[
-				// 	'nullable' => false
-				// 	],
-				// 	'Username'
-				// )
-				// ->addColumn(
-				// 	'password',
-				// 	\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-				// 	255,
-				// 	[
-				// 	'nullable' => false
-				// 	],
-				// 	'Password Hash'
-				// )
-				// ->addColumn(
-				// 	'created_at',
-				// 	\Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
-				// 	null,
-				// 	[
-				// 	'nullable' => false
-				// 	],
-				// 	'Vendor creation timestamp'
-				// )
-				// ->addColumn(
-				// 	'is_active',
-				// 	\Magento\Framework\DB\Ddl\Table::TYPE_BOOLEAN,
-				// 	null,
-				// 	[
-				// 	'nullable' => false, 
-				// 	'default' => 1
-				// 	],
-				// 	'Is Active'
-				// )
 				->addColumn(
-					'commission_perc',
+					'commission',
 					\Magento\Framework\DB\Ddl\Table::TYPE_FLOAT,
 					null,
 					[
 					'nullable' => false,
-					'unsigned' => true
+					'unsigned' => true,
+					'default' => 0
 					],
-					'Commission Percentage'
+					'Commission in %'
 				)
-				// ->addColumn(
-				// 	'contact_no',
-				// 	\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-				// 	20,
-				// 	[
-				// 	'nullable' => false,
-				// 	],
-				// 			'Contact Number'
-				// )
-				// ->addColumn(
-				// 	'email',
-				// 	\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-				// 	255,
-				// 	[
-				// 	'nullable' => false
-				// 	],
-				// 	'Email Address'
-				// )
-	 //      ->addColumn(
-			// 'address',
-			// \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-			// 255,
-			// [
-			// 'nullable' => false
-			// ],
-			// 'Vendor Address'
-	 //      )
-				->addColumn(
-					'role_id',
-					\Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
-					null,
-					[
-					'nullable' => true,
-					'unsigned' => true
-					],
-					'Vendor Role Id'
-				)
-	 //      ->addColumn(
-			// 'logo',
-			// \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-			// 255,
-			// [
-			// 'nullable' => true
-			// ],
-			// 'Vendor Logo'
-	 //      )
-	 //      ->addColumn(
-			// 'banner',
-			// \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-			// 255,
-			// [
-			// 'nullable' => true
-			// ],
-			// 'Vendor Banner'
-	 //      )
 				->addColumn(
 					'sort_order',
 					\Magento\Framework\Db\Ddl\Table::TYPE_INTEGER,
 					null,
 					[
-					'default' => 0,
+					'default' => null,
 					],
 					'Sort Order'
 				)
 				->addIndex(
 					$setup->getIdxName($vendorEntity . '_entity', ['sort_order']),
 					['sort_order']
-						)
-						->addIndex(
-					$setup->getIdxName($vendorEntity . '_entity', ['is_active']),
-					['is_active']
-				)
-				->addIndex(
-					$setup->getIdxName($vendorEntity . '_entity', ['email'], \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE),
-					['email'],
-					['type' => \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE]
-				)
-				->addForeignKey(
-					$setup->getFkName($vendorEntity . '_entity', 'role_id', $setup->getTable('authorization_role'), 'role_id'),
-					'role_id',
-					$setup->getTable('authorization_role'),
-					'role_id'
 				)
 				->addForeignKey(
 					$setup->getFkName($vendorEntity . '_entity', 'entity_id', $setup->getTable('admin_user'), 'user_id'),
@@ -210,8 +77,8 @@
 					[
 						'primary' => true,
 						'identity' => true, 
-							'unsigned' => true, 
-							'nullable' => false
+						'unsigned' => true, 
+						'nullable' => false
 					],
 					'Value ID'
 				)
@@ -251,7 +118,7 @@
 						\Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE
 					),
 					['entity_id', 'attribute_id'],
-			['type' => \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE]
+					['type' => \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE]
 				)
 				->addIndex(
 					$setup->getIdxName($vendorEntity . '_entity_int', ['attribute_id']),
@@ -263,8 +130,17 @@
 						$setup->getTable('eav_attribute'), 'attribute_id'
 					), 
 					'attribute_id',
-			$setup->getTable('eav_attribute'),
-			'attribute_id'
+					$setup->getTable('eav_attribute'),
+					'attribute_id'
+				)
+				->addForeignKey(
+					$setup->getFkName(
+						$vendorEntity . '_entity_int', 'entity_id', 
+						$setup->getTable($vendorEntity . '_entity'), 'entity_id'
+					), 
+					'entity_id',
+					$setup->getTable($vendorEntity . '_entity'),
+					'entity_id'
 				)
 				->setComment('Marketplace Vendor Integer Values Table');
 			$setup->getConnection()->createTable($vendorIntegerTypeTable);
@@ -320,7 +196,7 @@
 						\Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE
 					),
 					['entity_id', 'attribute_id'],
-			['type' => \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE]
+					['type' => \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE]
 				)
 				->addIndex(
 					$setup->getIdxName($vendorEntity . '_entity_decimal', ['attribute_id']),
@@ -332,8 +208,17 @@
 						$setup->getTable('eav_attribute'), 'attribute_id'
 					), 
 					'attribute_id',
-			$setup->getTable('eav_attribute'),
-			'attribute_id'
+					$setup->getTable('eav_attribute'),
+					'attribute_id'
+				)
+				->addForeignKey(
+					$setup->getFkName(
+						$vendorEntity . '_entity_decimal', 'entity_id', 
+						$setup->getTable($vendorEntity . '_entity'), 'entity_id'
+					), 
+					'entity_id',
+					$setup->getTable($vendorEntity . '_entity'),
+					'entity_id'
 				)
 				->setComment('Marketplace Vendor Decimal Values Table');
 			$setup->getConnection()->createTable($vendorDecimalTypeTable);
@@ -389,7 +274,7 @@
 						\Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE
 					),
 					['entity_id', 'attribute_id'],
-			['type' => \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE]
+					['type' => \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE]
 				)
 				->addIndex(
 					$setup->getIdxName($vendorEntity . '_entity_text', ['attribute_id']),
@@ -401,8 +286,17 @@
 						$setup->getTable('eav_attribute'), 'attribute_id'
 					), 
 					'attribute_id',
-			$setup->getTable('eav_attribute'),
-			'attribute_id'
+					$setup->getTable('eav_attribute'),
+					'attribute_id'
+				)
+				->addForeignKey(
+					$setup->getFkName(
+						$vendorEntity . '_entity_text', 'entity_id', 
+						$setup->getTable($vendorEntity . '_entity'), 'entity_id'
+					), 
+					'entity_id',
+					$setup->getTable($vendorEntity . '_entity'),
+					'entity_id'
 				)
 				->setComment('Marketplace Vendor Text Values Table');
 			$setup->getConnection()->createTable($vendorTextTypeTable);
@@ -458,7 +352,7 @@
 						\Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE
 					),
 					['entity_id', 'attribute_id'],
-			['type' => \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE]
+					['type' => \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE]
 				)
 				->addIndex(
 					$setup->getIdxName($vendorEntity . '_entity_datetime', ['attribute_id']),
@@ -470,8 +364,17 @@
 						$setup->getTable('eav_attribute'), 'attribute_id'
 					), 
 					'attribute_id',
-			$setup->getTable('eav_attribute'),
-			'attribute_id'
+					$setup->getTable('eav_attribute'),
+					'attribute_id'
+				)
+				->addForeignKey(
+					$setup->getFkName(
+						$vendorEntity . '_entity_datetime', 'entity_id', 
+						$setup->getTable($vendorEntity . '_entity'), 'entity_id'
+					), 
+					'entity_id',
+					$setup->getTable($vendorEntity . '_entity'),
+					'entity_id'
 				)
 				->setComment('Marketplace Vendor Datetime Values Table');
 			$setup->getConnection()->createTable($vendorDatetimeTypeTable);
@@ -479,6 +382,85 @@
 			//TYPE VARCHAR
 			$vendorVarcharTypeTable = $setup->getConnection()
 				->newTable($setup->getTable($vendorEntity . '_entity_varchar'))
+				->addColumn(
+					'value_id',
+					\Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+					null,
+					[
+						'primary' => true,
+						'identity' => true, 
+						'unsigned' => true, 
+						'nullable' => false
+					],
+					'Value ID'
+				)
+				->addColumn(
+					'attribute_id',
+					\Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+					null,
+					[
+						'nullable' => false,
+						'unsigned' => true,
+					],
+					'Attribute ID'
+				)
+				->addColumn(
+					'entity_id',
+					\Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+					null,
+					[
+						'nullable' => false,
+						'unsigned' => true
+					],
+					'Entity ID'
+				)
+				->addColumn(
+					'value',
+					\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+					255,
+					[
+						'nullable' => true
+					],
+					'Value'
+				)
+				->addIndex(
+					$setup->getIdxName(
+						$vendorEntity . '_entity_varchar', 
+						['entity_id', 'attribute_id'], 
+						\Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE
+					),
+					['entity_id', 'attribute_id'],
+					['type' => \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE]
+				)
+				->addIndex(
+					$setup->getIdxName($vendorEntity . '_entity_varchar', ['attribute_id']),
+					['attribute_id']
+				)
+				->addForeignKey(
+					$setup->getFkName(
+						$vendorEntity . '_entity_varchar', 'attribute_id', 
+						$setup->getTable('eav_attribute'), 'attribute_id'
+					), 
+					'attribute_id',
+					$setup->getTable('eav_attribute'),
+					'attribute_id'
+				)
+				->addForeignKey(
+					$setup->getFkName(
+						$vendorEntity . '_entity_varchar', 'entity_id', 
+						$setup->getTable($vendorEntity . '_entity'), 'entity_id'
+					), 
+					'entity_id',
+					$setup->getTable($vendorEntity . '_entity'),
+					'entity_id'
+				)
+				->setComment('Marketplace Vendor Varchar Values Table');
+			$setup->getConnection()->createTable($vendorVarcharTypeTable);
+
+			$setup->endSetup();
+		}
+	}
+$setup->getTable($vendorEntity . '_entity_varchar'))
 				->addColumn(
 					'value_id',
 					\Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
